5bc96e267bb6c1dae2a6e1021b7a26ce22976d8c
Omkar Nagare <68635743+Jumpback@users.noreply.github.com>
2022-05-03 20:38:36 +0530

    Time: 8 ms (38.74%), Space: 43.2 MB (72.42%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -5,3 +5,19 @@ class Solution {
         return nums[n - k];
     }
 }
+
+// PriorityQueue<Integer>pq=new PriorityQueue<>();
+        
+//         for(int i=0;i<k;i++)
+//         {
+//             pq.add(nums[i]);
+//         }
+//         for(int i=k;i<nums.length;i++)
+//         {
+//             if(pq.peek()<nums[i])
+//             {
+//                 pq.remove();
+//                 pq.add(nums[i]);
+//             }
+//         }
+//        return pq.peek();
