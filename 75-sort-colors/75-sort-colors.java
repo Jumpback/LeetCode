8ae3063a64945ec6182c802c601ab043dac3b4e4
Omkar Nagare <68635743+Jumpback@users.noreply.github.com>
2022-05-04 10:26:01 +0530

    Time: 1 ms (33.59%), Space: 42.8 MB (28.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+              swap(nums,low++,mid++);
+            else if(nums[mid]==2)
+                 swap(nums,high--,mid);
+            else
+                mid++;
+        }
+    
     }
+        public void swap(int[] nums, int i, int j) {
+	    int temp = nums[i];
+	    nums[i] = nums[j];
+	    nums[j] = temp;
+      }
 }
+
+
+//Using Dutch National Flag Algorithm.!
